c0854c0bf6cb0288d75ea96ffcbe079df3d34047
KindrakevychNatali <natali.kindrakevych@gmail.com>
2023-12-12 12:10:25 +0300

    updating webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
 'use strict';
 
-let path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './js/script.js',
+  mode: 'development', // або 'production' або 'none'
+  entry: './js/script.js', // Замініть це на шлях до вашого основного модуля
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/js'
+    path: path.resolve(__dirname, 'build'),
   },
-  watch: true,
-
-  devtool: "source-map",
-//babel configs
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // Додайте babel-loader для обробки JavaScript
           options: {
-            presets: [['@babel/preset-env', {
-                debug: true,
-                corejs: 3,
-                useBuiltIns: "usage"
-            }]]
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Додайте підтримку для обробки CSS
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', // Додайте підтримку для обробки зображень
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // шлях до вашого вихідного HTML файлу
+      filename: 'index.html', // ім'я згенерованого HTML файлу
+    }),
+  ],
 };
+//інсталювати обов'язкові залежості (npm install webpack webpack-cli babel-loader @babel/core @babel/preset-env style-loader css-loader html-webpack-plugin--save-dev);
